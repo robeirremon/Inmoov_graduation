debf4aa2a0099f8f1417d30a4637cf18b2ebbfd6
rami <ramiwafik97@hotmail.com>
2020-05-25 01:10:54 +0200

    detect

--- Perception/detect.py
+++ Perception/detect.py
@@ -36,7 +36,7 @@ def detect():
         data_to_send.data = all_values # assign the array with the value you want to send
         pub.publish(data_to_send)
 
-        real_values_old = real_values
+        old_real_values = real_values
         
     #     if cv2.waitKey(1) & 0xFF == ord('q'):
     #         break
